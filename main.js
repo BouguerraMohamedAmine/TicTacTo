ec8c690a23ab2ba7d35d522565b1dcd0ad12a83f
BouguerraMohamedAmine <med.bouguerraa@gmail.com>
2023-06-07 13:24:12 +0100

    js

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
+function myfunction(){
+    var b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    b1 = document.getElementById("b1").value;
+    b2 = document.getElementById("b2").value;
+    b3 = document.getElementById("b3").value;
+    b4 = document.getElementById("b4").value;
+    b5 = document.getElementById("b5").value;
+    b6 = document.getElementById("b6").value;
+    b7 = document.getElementById("b7").value;
+    b8 = document.getElementById("b8").value;
+    b9 = document.getElementById("b9").value;
+  
+  
+    if ((b1 == "x" || b1 == "X") && (b2 == "x" || b2 == "X") && (b3 == "x" || b3 == "X")) {
+        document.getElementById("print")
+            .innerHTML = "Player X won";
+        document.getElementById("b4").disabled = true;
+        document.getElementById("b5").disabled = true;
+        document.getElementById("b6").disabled = true;
+        document.getElementById("b7").disabled = true;
+        document.getElementById("b8").disabled = true;
+        document.getElementById("b9").disabled = true;
+        window.alert("Player X won");
+}
+else if ((b1 == "x" || b1 == "X") && (b4 == "x" || b4 == "X") && (b7 == "x" || b7 == "X")) {
+        document.getElementById("print")
+            .innerHTML = "Player X won";
+        document.getElementById("b2").disabled = true;
+        document.getElementById("b3").disabled = true;
+        document.getElementById("b5").disabled = true;
+        document.getElementById("b6").disabled = true;
+        document.getElementById("b8").disabled = true;
+        document.getElementById("b9").disabled = true;
 
-    var blocks =  b1, b2, b3, b4, b5, b6, b7, b8, b9;
-    b1 = document.getElementById("b1");
-    b2 = document.getElementById("b2");
-    b3 = document.getElementById("b3");
-    b4 = document.getElementById("b4");
-    b5 = document.getElementById("b5");
-    b6 = document.getElementById("b6");
-    b7 = document.getElementById("b7");
-    b8 = document.getElementById("b8");
-    b9 = document.getElementById("b9");
+               window.alert("Player X won");
+}
+else if ((b1 == "x" || b1 == "X" ) && (b5 == "x" || b5 == 5 ) && ( b9 == "x" || b9 == b9 )) {
+        document.getElementById("print")
+
+} 
+    
