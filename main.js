eb3986dade0ef32cab3633ab0a4b11e9dce87e2d
BouguerraMohamedAmine <med.bouguerraa@gmail.com>
2023-06-07 10:36:54 +0100

    main

---
+++ main.js
@@ -0,0 +1,11 @@
+
+    var blocks =  b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    b1 = document.getElementById("b1");
+    b2 = document.getElementById("b2");
+    b3 = document.getElementById("b3");
+    b4 = document.getElementById("b4");
+    b5 = document.getElementById("b5");
+    b6 = document.getElementById("b6");
+    b7 = document.getElementById("b7");
+    b8 = document.getElementById("b8");
+    b9 = document.getElementById("b9");
