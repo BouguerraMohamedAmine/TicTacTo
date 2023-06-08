3eb2a9e2c9729eb49bfe6057f1b0ee4121af34f8
BouguerraMohamedAmine <med.bouguerraa@gmail.com>
2023-06-08 09:42:01 +0100

    donnee

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 
-// Function called whenever user tab on any box
+
 function myfunc() {
   
-    // Setting DOM to all boxes or input field
     var b1, b2, b3, b4, b5, b6, b7, b8, b9;
     b1 = document.getElementById("b1").value;
     b2 = document.getElementById("b2").value;
@@ -113,10 +112,7 @@ function myfunc() {
         window.alert('Player X won');
     }
   
-    // Checking of Player X finish
-    // Checking for Player 0 starts, Is player 0 won or
-    // not and after that disabled all the other fields
-    else if ((b1 == '0' || b1 == '0') && (b2 == '0' || 
+else if ((b1 == '0' || b1 == '0') && (b2 == '0' || 
         b2 == '0') && (b3 == '0' || b3 == '0')) {
         document.getElementById('print')
             .innerHTML = "Player 0 won";
@@ -213,8 +209,7 @@ function myfunc() {
         window.alert('Player 0 won');
     }
   
-    // Checking of Player 0 finish
-    // Here, Checking about Tie
+
     else if ((b1 == 'X' || b1 == '0') && (b2 == 'X' 
         || b2 == '0') && (b3 == 'X' || b3 == '0') && 
         (b4 == 'X' || b4 == '0') && (b5 == 'X' || 
@@ -227,7 +222,7 @@ function myfunc() {
     }
     else {
   
-        // Here, Printing Result
+
         if (flag == 1) {
             document.getElementById('print')
                 .innerHTML = "Player X Turn";
@@ -241,7 +236,7 @@ function myfunc() {
 
 
   
-// Function to reset game
+
 function myfunc_2() {
     location.reload();
     document.getElementById('b1').value = '';
@@ -256,8 +251,7 @@ function myfunc_2() {
   
 }
   
-// Here onwards, functions check turn of the player 
-// and put accordingly value X or 0
+
 flag = 1;
 function myfunc_3() {
     if (flag == 1) {
